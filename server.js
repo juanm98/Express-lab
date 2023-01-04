515c0fdd95119a36cb1b9c4a90b2c3717376e496
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-04 09:16:01 -0400

    Add views directory

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/students', function (req, res) {
     res.send('<h1>hello, students</h1>')
 })
 
+app.get('/home', function(req, res) {
+    res.render('home')
+})
+
 // Tell the app to listen on port 3000
 
 app.listen(3000, function() {
